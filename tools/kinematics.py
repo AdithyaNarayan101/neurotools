d7162e54b7b3b8b0cdbc36d6c332dcccc83adb9b
Adithya Narayan <adithyanarayan101@gmail.com>
2024-10-23 15:30:40 -0400

    Fixed minor issue. Tested on one session of Pu/Sa data preprocessing

--- tools/kinematics.py
+++ tools/kinematics.py
@@ -1,16 +1,26 @@
 '''
-Functions to analyze kinematics of trajectories (Joystick, Arm, Eyes) 
+Module to analyze kinematics of trajectories (Joystick, Arm, Eyes) 
+
+Compute Reaction Times, Peak Speed, Velocity Profiles from (x,y,t) trajectory data
 
 '''
-
-def compute_kinematics(trajectory, filter='butter',low_pass_threshold=10, velocity_threshold = 0.2, threshold_type='trialwise', fs = 1000, plot_diagnostic=False):
+import numpy as np
+def butter_lowpass_filter(data, cutoff, fs, order=2):
+    from scipy.signal import butter,filtfilt
+    nyq = 0.5 * fs # Nyquist Frequency
+    normal_cutoff = cutoff / nyq
+    # Get the filter coefficients 
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+def compute_kinematics(trajectory, low_pass_filter='butter',low_pass_threshold=10, velocity_threshold = 0.2, threshold_type='trialwise', fs = 1000, plot_diagnostic=False):
     '''
     Takes in trajectories (joystick/eye/arm) and outputs kinematics like Response Time, Velocity Profiles, Peak Velocity (in ms and corresponding units)
     '''
 
     dist_from_center = np.sqrt(trajectory[0,:]**2 + trajectory[1,:]**2)
 
-    if(filter == 'butter'):
+    if(low_pass_filter == 'butter'):
         dist_from_center_filtered = butter_lowpass_filter(dist_from_center, low_pass_threshold, fs) # low pass filter
     else:
         raise ExceptionType("Error message")
@@ -20,22 +30,22 @@ def compute_kinematics(trajectory, filter='butter',low_pass_threshold=10, veloci
     # Unsure what the next two lines are doing - must test! (Why interpolate?)
     oldx = np.arange(len(vel)) + (fs / 1000 / 2)
     newx = np.arange(len(vel))
-    interp_vel = np.interp(newx, oldx, dxdt)
+    interp_vel = np.interp(newx, oldx, vel)
     peak_vel = np.max(interp_vel)
     if(threshold_type=='trialwise'):
-        velocity_thresh_trial = peak_vel*velocity_thresh # Threshold for determining RT set separately for each trial
+        velocity_threshold_trial = peak_vel*velocity_threshold # Threshold for determining RT set separately for each trial
     elif(threhold_type=='common'):
-        velocity_thresh_trial = velocity_thresh # This threshold should've been computed across trials
+        velocity_threshold_trial = velocity_threshold # This threshold should've been computed across trials
 
 
-    response_time = np.argwhere(interp_vel > velocity_thresh_trial)[0][0] / fs * 1000 # ms, locked to target onset
+    response_time = np.argwhere(interp_vel > velocity_threshold_trial)[0][0] / fs * 1000 # ms, locked to target onset
     peak_vel_time =    np.argwhere(interp_vel == peak_vel)[0][0] / fs * 1000 # ms, locked to target onset
         
-    kinematics = {'Response Time':response_time, 'peak_velocity':peak_vel, 'peak_velocity_time':peak_vel_time, 'velocity_profile':interp_vel }
+    kinematics = {'Response Time':response_time, 'Peak Velocity':peak_vel, 'Peak Velocity Time':peak_vel_time, 'Velocity Profile':interp_vel }
 
     if plot_diagnostic: # Plots velocity profile + RT for this trial
         fig=go.Figure(go.Scatter(x=np.arange(0,len(interp_vel)),y=interp_vel))
         fig.add_trace(go.Scatter(x=[response_time,response_time],y=[np.min(interp_vel),np.max(interp_vel)],mode='lines',line=dict(width=4,color='black')))
         fig.show()
 
-return kinematics
+    return kinematics
