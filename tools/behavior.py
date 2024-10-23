2c451b100163d508f20b0b8609a18a3593a3a20e
Adithya Narayan <adithyanarayan101@gmail.com>
2024-10-23 15:31:08 -0400

    created a behavior analysis script.

---
+++ tools/behavior.py
@@ -0,0 +1,10 @@
+'''
+Module to analyze behavior
+
+- Compute basic behavioral metrics like mean success rates, hit rates, false alarm rates, motor accuracy 
+
+'''
+
+class behavior_metrics:
+    
+    def __
